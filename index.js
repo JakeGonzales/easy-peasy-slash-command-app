26f1de6ccdc6d3db383bbd4f4878a1b67771a435
Jake Gonzales <jake@twoofthree.com>
2017-08-19 17:16:12 +0800

    WIP retrying original example code

--- index.js
+++ index.js
@@ -88,10 +88,9 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/lfg": //handle the `/echo` slash command. We might have others assigned to this app too!
+        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
-            // Link for this slash command is https://waraiderslfgslash.localtunnel.me/oauth
 
             // but first, let's make sure the token matches!
             if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
@@ -99,14 +98,13 @@ controller.on('slash_command', function (slashCommand, message) {
             // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
-                    "I output your LFG requests! " +
-                    "Try typing `/lfg pvp` to see.");
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
                 return;
             }
 
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
-
             slashCommand.replyPublic(message, "1", function() {
                 slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
             });
@@ -119,4 +117,3 @@ controller.on('slash_command', function (slashCommand, message) {
 
 })
 ;
-
